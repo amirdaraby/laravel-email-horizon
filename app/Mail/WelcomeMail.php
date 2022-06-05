58196013db1ce7918d196ca7778266f59d20a32b
amirdaraby <awmir@outlook.com>
2022-06-05 16:22:07 +0430

    created welcome mail

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function build()
+    {
+        sleep(2);
+        return $this->view('email');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    \Illuminate\Support\Facades\Mail::to('test@example.com')->send(new \App\Mail\WelcomeMail());
     return view('welcome');
 });
